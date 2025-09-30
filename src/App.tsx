d3775c12e4f86698ad51209bdd03371ca5b74bf6
Khoi Vu <khoi.vu@nextchain.kr>
2025-09-30 11:41:32 +0700

    fix: move modals-provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import router from '@/router/router'
 import {RouterProvider} from 'react-router-dom'
 import './i18n/i18n'
-import AppProvider from './providers/AppProvider'
+import AppProvider from './providers/app-provider'
+import ModalsProvider from './providers/modals-provider'
 
 function App() {
 	return (
 		<AppProvider>
-			<RouterProvider router={router} />
+			<ModalsProvider>
+				<RouterProvider router={router} />
+			</ModalsProvider>
 		</AppProvider>
 	)
 }
