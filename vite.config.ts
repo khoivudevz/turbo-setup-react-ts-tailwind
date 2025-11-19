3de8c05e5324ba3198df210e99f37c4df659866d
Khoi Vu <khoi.vu@nextchain.kr>
2025-11-19 14:40:45 +0700

    feat: react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import preload from 'vite-plugin-preload'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), preload(), tailwindcss()],
+	plugins: [
+		react({
+			babel: {
+				plugins: ['babel-plugin-react-compiler'],
+			},
+		}),
+		preload(),
+		tailwindcss(),
+	],
 	resolve: {
 		alias: [{find: '@', replacement: '/src'}],
 	},
